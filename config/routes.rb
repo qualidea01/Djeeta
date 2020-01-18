7774343a89fc4d6e492804a28482a10633a40228
yuto tomita <qualidea01@gmail.com>
2020-01-18 16:14:36 +0900

    セッションモデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get 'users/new'
+  get 'users/create'
+  get 'users/me'
+  get 'home/index'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  root 'home#index'
 end
